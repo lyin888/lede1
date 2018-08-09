851d505310495942de6441e2f816b0d9526bb0be
dz <505575853@qq.com>
2018-08-09 19:52:24 +0800

    回退

--- package/my/luci-app-shadowsocksr-alex/files/usr/lib/lua/luci/model/cbi/shadowsocksr/general.lua
+++ package/my/luci-app-shadowsocksr-alex/files/usr/lib/lua/luci/model/cbi/shadowsocksr/general.lua
@@ -4,32 +4,19 @@ References:
  https://github.com/ravageralpha/my_openwrt_mod  - by RA-MOD
  http://www.v2ex.com/t/139438  - by imcczy
  https://github.com/rssnsj/network-feeds  - by Justin Liu
- https://github.com/505575853  - by dz
 ]]--
 
 local fs = require "nixio.fs"
-local NXFS = require "nixio.fs"
-local WLFS = require "nixio.fs"
-local SYS  = require "luci.sys"
-local http = luci.http
-local ucursor = require "luci.model.uci".cursor()
-
 
 local state_msg = ""
 local ssr_redir_on = (luci.sys.call("pidof ssr-redir > /dev/null") == 0)
 local ss_redir_on = (luci.sys.call("pidof ss-redir > /dev/null") == 0)
-local v2ray_on = (luci.sys.call("pidof v2ray > /dev/null") == 0)
-local brook_on = (luci.sys.call("pidof brook > /dev/null") == 0)
 local redsocks2_on = (luci.sys.call("pidof redsocks2 > /dev/null") == 0)
 
 if ssr_redir_on then	
 	state_msg = "<b><font color=\"green\">" .. translate("SSR is Running") .. "</font></b>"
 elseif ss_redir_on then
 	state_msg = "<b><font color=\"green\">" .. translate("SS is Running") .. "</font></b>"
-elseif v2ray_on then                                                                                            
-	state_msg = "<b>  <font color=\"green\">" .. translate("V2ray is Running") .. "</font></b>"
-elseif brook_on then                                                                                            
-	state_msg = "<b>  <font color=\"green\">" .. translate("Brook is Running") .. "</font></b>"
 elseif redsocks2_on then
 	state_msg = state_msg .. "<b>  <font color=\"green\">" .. translate("Redsocks2 is Running") .. "</font></b>"
 else
@@ -48,13 +35,6 @@ s.anonymous = true
 switch = s:option(Flag, "enabled", translate("Enable"))
 switch.rmempty = false
 
-tool = s:option(ListValue, "tool", translate("Proxy Tool"))
-tool:value("ShadowsocksR")
-tool:value("Shadowsocks")
-tool:value("v2ray")
-tool:value("Brook")
-tool:value("Redsocks2")
-
 server = s:option(Value, "server", translate("Server Address"))
 server.optional = false
 server.datatype = "host"
@@ -64,6 +44,11 @@ server_port = s:option(Value, "server_port", translate("Server Port"))
 server_port.datatype = "range(1,65535)"
 server_port.optional = false
 server_port.rmempty = false
+
+tool = s:option(ListValue, "tool", translate("Proxy Tool"))
+tool:value("ShadowsocksR")
+tool:value("Shadowsocks")
+tool:value("Redsocks2")
 
 red_type=s:option(ListValue,"red_type",translate("Proxy Server Type"))
 red_type:value("socks5",translate("Socks5"))
@@ -76,9 +61,6 @@ username = s:option(Value, "username", translate("Proxy User Name"))
 username:depends({tool="Redsocks2"})
 
 password = s:option(Value, "password", translate("Password"))
-password:depends("tool","ShadowsocksR")
-password:depends("tool","Shadowsocks")
-password:depends("tool","Brook")
 password.password = true
 
 
@@ -136,8 +118,6 @@ obfs_param= s:option(Value, "obfs_param", translate("Obfs Param"),
 	translate("leave it empty is well"))
 obfs_param:depends({tool="ShadowsocksR"})
 
-
-
 enable_local = s:option(Flag, "enable_local", translate("Enable ssr-local") ,translate("Open ssr-local port as well"))
 enable_local.rmempty = false
 enable_local:depends("tool","ShadowsocksR")
@@ -145,68 +125,6 @@ enable_local:depends("tool","Shadowsocks")
 ssr_local_port = s:option(Value, "ssr_local_port", translate("ssr-local port"))
 ssr_local_port:depends("enable_local","1")
 ssr_local_port.default="1080"
-
-use_conf_file = s:option(Flag, "use_conf_file", translate("Use Config File"), translate("Use Config File").."<br>使用教程请<a href='https://github.com/505575853/luci-app-v2ray/blob/master/README.md'>点击这里</a><br><br>")
-use_conf_file:depends("tool","v2ray")
-use_conf_file.rmempty = false
-
-if nixio.fs.access("/usr/bin/v2ray/v2ctl") then
-	conf_file_type = s:option(ListValue, "conf_file_type", translate("Config File Type"))
-	conf_file_type:value("pb","Protobuf")
-else 
-	conf_file_type = s:option(ListValue, "conf_file_type", translate("Config File Type"), translate("Warning: Can't find v2ctl. You can only choose Protobuf."))
-end
-conf_file_type:value("json","JSON")
-conf_file_type:depends("use_conf_file", 1)
-
-conf_file_path = s:option(Value, "conf_file_path", translate("Config File Path"),
-	translate("If you choose to upload a new file, please do not modify and this configuration will be overwritten automatically."))
-conf_file_path:depends("use_conf_file", 1)
-
-upload_conf = s:option(FileUpload, "")
-upload_conf.template = "cbi/other_upload2"
-upload_conf:depends("use_conf_file", 1)
-
-um = s:option(DummyValue, "", nil)
-um.template = "cbi/other_dvalue"
-um:depends("use_conf_file", 1)
-
-
-
-local conf_dir, fd
-conf_dir = "/etc/v2ray/"
-nixio.fs.mkdir(conf_dir)
-http.setfilehandler(
-	function(meta, chunk, eof)
-		if not fd then
-			if not meta then return end
-
-			if	meta and chunk then fd = nixio.open(conf_dir .. meta.file, "w") end
-
-			if not fd then
-				um.value = translate("Create upload file error.")
-				return
-			end
-		end
-		if chunk and fd then
-			fd:write(chunk)
-		end
-		if eof and fd then
-			fd:close()
-			fd = nil
-			um.value = translate("File saved to") .. ' "/etc/v2ray/' .. meta.file .. '"'
-			ucursor:set("v2ray","v2ray","conf_file_path","/etc/v2ray/" .. meta.file)
-			ucursor:commit("v2ray")
-		end
-	end
-)
-
-if luci.http.formvalue("upload") then
-	local f = luci.http.formvalue("ulfile")
-	if #f <= 0 then
-		um.value = translate("No specify upload file.")
-	end
-end
 
 s:option(Flag, "more", translate("More Options"),
 	translate("Options for advanced users"))
