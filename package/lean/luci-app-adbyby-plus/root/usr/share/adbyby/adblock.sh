b63f2e71296e29f1c689b93a9e141fbf4be0c9e6
505575853 <505575853@qq.com>
2018-09-25 20:46:02 +0800

    mod1.6.0

--- package/lean/luci-app-adbyby-plus/root/usr/share/adbyby/adblock.sh
+++ package/lean/luci-app-adbyby-plus/root/usr/share/adbyby/adblock.sh
@@ -3,11 +3,12 @@
 rm -f /tmp/dnsmasq.adblock
 
 wget-ssl --no-check-certificate -O- https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > /tmp/dnsmasq.adblock
+wget-ssl --no-check-certificate -O- http://tools.yiclear.com/ChinaList2.0.txt | grep ^||[^*]*^$ | sed -e 's:||:address=/:' -e 's:^:/0.0.0.0:' > /tmp/dnsmasq.adblock
+wget-ssl --no-check-certificate -O- https://easylist.to/easylist/easylist.txt | grep ^||[^*]*^$ | sed -e 's:||:address=/:' -e 's:^:/0.0.0.0:' >> /tmp/dnsmasq.adblock
+wget-ssl --no-check-certificate -O- https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/ABP-FX.txt | grep ^||[^*]*^$ | sed -e 's:||:address=/:' -e 's:^:/0.0.0.0:' >> /tmp/dnsmasq.adblock
 if [ -s "/tmp/dnsmasq.adblock" ];then
 	sed -i '/youku.com/d' /tmp/dnsmasq.adblock
-	if ( ! cmp -s /tmp/dnsmasq.adblock /usr/share/adbyby/dnsmasq.adblock );then
-		mv /tmp/dnsmasq.adblock /usr/share/adbyby/dnsmasq.adblock	
-	fi	
+	mv /tmp/dnsmasq.adblock /usr/share/adbyby/dnsmasq.adblock	
 fi
 
 sh /usr/share/adbyby/adupdate.sh
